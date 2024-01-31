c5fff5fc95fe9b2aa78bcfbeac626db0b5eecde2
Kevin Trevis <ketrevis@bess-f4r2s9.clusters.42paris.fr>
2024-01-31 15:12:51 +0100

    echo flag done

--- ft_echo.c
+++ ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 16:36:04 by ketrevis          #+#    #+#             */
-/*   Updated: 2024/01/31 14:29:50 by ketrevis         ###   ########.fr       */
+/*   Updated: 2024/01/31 15:10:14 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,21 +29,40 @@ static void	normal_print(char *str, t_env *env_list)
 		write(1, str, ft_strlen(str));
 }
 
+int	is_only_n(char *str)
+{
+	int	i;
+
+	i = 1;
+	while (str[i])
+	{
+		if (str[i] != 'n')
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
 void	ft_echo(char **split, t_env *env_list)
 {
 	uint	i;
 	bool	flag;
 
-	(void)env_list;
 	flag = false;
 	i = 1;
+	while (split[i] && !ft_strncmp(split[i], "-n", 2) && is_only_n(split[i]))
+	{
+		flag = true;
+		i++;
+	}
 	while (split[i])
 	{
 		if (split[i][0] == '\'')
 			write(1, split[i] + 1, ft_strlen(split[i]) - 2);
 		else
 			normal_print(split[i], env_list);
-		write(1, " ", 1);
+		if (split[i + 1])
+			write(1, " ", 1);
 		i++;
 	}
 	if (!flag)
