ecd6ab26ab853b8bfd1e5b3adb5bbb1e3f9d4c18
Kevin Trevis <ketrevis@student.42.fr>
2024-02-02 11:22:25 +0100

    str replace norme

--- srcs/str_replace.c
+++ srcs/str_replace.c
@@ -6,7 +6,7 @@
 /*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 21:32:02 by ketrevis          #+#    #+#             */
-/*   Updated: 2024/02/02 11:10:15 by ketrevis         ###   ########.fr       */
+/*   Updated: 2024/02/02 11:22:06 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,10 +34,11 @@ char	*str_replace(char *str, char *old, char *new)
 	{
 		if (str[i] == '\'')
 			in_simple_quote = !in_simple_quote;
-		if (str[i] == '$' && !ft_strncmp(str + i, old, ft_strlen(old)) && !in_simple_quote)
+		if (str[i] == '$' && !ft_strncmp(str + i, old, ft_strlen(old))
+			&& !in_simple_quote)
 		{
 			j += ft_strlcpy(new_str + j, new, ft_strlen(new) + 1);
-			j += ft_strlcpy(new_str + j, str + i + ft_strlen(old), ft_strlen(str));
+			ft_strlcpy(new_str + j, str + i + ft_strlen(old), ft_strlen(str));
 			return (new_str);
 		}
 		new_str[j++] = str[i++];
