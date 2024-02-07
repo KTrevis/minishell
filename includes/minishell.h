c988e3a513b93f7bab8802dbb8d95fcfca38dae4
Kevin Trevis <ketrevis@bess-f4r2s10.clusters.42paris.fr>
2024-02-07 15:41:21 +0100

    ordered function header

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ketrevis <ketrevis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/30 15:20:06 by ketrevis          #+#    #+#             */
-/*   Updated: 2024/02/07 15:37:32 by ketrevis         ###   ########.fr       */
+/*   Updated: 2024/02/07 15:40:54 by ketrevis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ typedef struct s_split
 	struct s_split	*next;
 }	t_split;
 
-enum input_parse_ret
+enum e_input_parse_ret
 {
 	EXIT,
 	EMPTY_INPUT,
@@ -40,15 +40,15 @@ enum input_parse_ret
 };
 
 t_split	**shell_split(char **old_split);
+t_env	*store_env(char **env);
+t_env	*add_env_node(t_env *head, char *name, char *value);
 void	free_env_list(t_env *env);
 void	display_env(t_env *env);
 void	input(t_env *env);
 void	free_shell_split(t_split *split);
-t_env	*store_env(char **env);
-t_env	*add_env_node(t_env *head, char *name, char *value);
-int		parse_input(char *input, t_env *env);
 char	*str_replace(char *str, char *old, char *new, int old_index);
 char	*get_var_value(t_env *head, char *name);
 char	*replace_var_names(char *input, t_env *env);
+int		parse_input(char *input, t_env *env);
 
 #endif
